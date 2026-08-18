8018e47546f88eda5adc7c0b6039f952c4b142a2
IgnacioCG07 <ignacioacunag@alu.unach.cl>
2026-08-18 12:48:28 -0400

    Actividad 4 - Formulario de Validación

--- src/components/Padre.vue
+++ src/components/Padre.vue
@@ -3,16 +3,19 @@
         <Hijo mensaje="Hola desde el padre" />
         <HijoBoton @saludo="mostrarSaludo" />
         <p v-if="mensaje">{{ mensaje }}</p>
+        <Contacto />
     </div>
 </template>
 <script>
 import Hijo from './Hijo.vue'
 import HijoBoton from './HijoBoton.vue'
+import Contacto from './Contacto.vue'
 
 export default {
     components: {
         Hijo,
-        HijoBoton
+        HijoBoton,
+        Contacto
     },
     data() {
         return {

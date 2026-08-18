4b94db222fe2ddb537d57eb1ba1683d794548fa7
IgnacioCG07 <ignacioacunag@alu.unach.cl>
2026-08-18 12:36:10 -0400

    Actividad 3 - Emit desde hijo

--- src/components/Padre.vue
+++ src/components/Padre.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
         <Hijo mensaje="Hola desde el padre" />
+        <HijoBoton @saludo="mostrarSaludo" />
+        <p v-if="mensaje">{{ mensaje }}</p>
     </div>
 </template>
 <script>
 import Hijo from './Hijo.vue'
+import HijoBoton from './HijoBoton.vue'
+
 export default {
     components: {
-        Hijo
+        Hijo,
+        HijoBoton
+    },
+    data() {
+        return {
+            mensaje: ''
+        }
+    },
+    methods: {
+        mostrarSaludo() {
+            this.mensaje = 'Saludo recibido desde el hijo';
+        }
     }
 }
 </script>

fc4023dca546c083d69ce9fd42d3581bbfe4d5d8
IgnacioCG07 <ignacioacunag@alu.unach.cl>
2026-08-19 17:27:51 -0400

    Actividad 4 - Formulario de Validación

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div id="app">
         <Padre />
+        <hr />
+        <Tareas />
     </div>
 </template>
 <script>
 import Padre from './components/Padre.vue'
+import Tareas from './components/Tareas.vue'
 export default {
     components: {
-        Padre
+        Padre,
+        Tareas
     }
 }
 </script>

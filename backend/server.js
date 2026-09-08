ba5d68b99fdae7f58cdfa93efbd4e9627fed2399
IgnacioCG07 <ignacioacunag@alu.unach.cl>
2026-09-08 12:15:50 -0300

    Actividad 9 - Preparacion para recibir JSON

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const servicios = require('./data/servicios')
 const app = express()
 const PORT = 3000
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Servidor de empresa funcionando correctamente')
